efae526bfcccaaba0747b6859d0e99f76a69b0e3
Kaushal <kaushaldhakal90@gmail.com>
2023-01-02 00:27:21 +0545

    pagination completed

--- src/scrap/scrap.service.ts
+++ src/scrap/scrap.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
+const fs = require('fs');
 import * as puppeteer from 'puppeteer';
 let csvData;
 @Injectable()
 export class ScrapService {
   async getDataViaPuppeteer(category: string = '') {
-    // this.sayHi();
-    console.log("here");
+    let arr = [];
    let pageNo =1;
     try {
       const URL = `https://www.coursera.org/search?query=${category}`;
@@ -22,96 +22,120 @@ export class ScrapService {
       await page.waitForSelector(selectorStr);
       const totalPages = await this.getPageLimit(page);
       console.log("total Number of required loops ===>",totalPages);
-      
-      const course = await this.extractPageData(page);
-      // console.log("COURSES===>",course);
-      
-      await this.handleNext(page);
-      // console.log("Result =====>",result);
+      //looping over each page and extracting data.
+      (async ()=>{
+        let finalResult = [];
+        for (let i = 0; i <= totalPages; i++) {
+          console.log("Breaks on which loop==>",i);
+          let dataEachPage = [];
+          if(i == totalPages){
+            dataEachPage = await this.extractPageData(page);
+            finalResult = finalResult?.concat(dataEachPage);
+            return;
+          }else{
+            dataEachPage = await this.extractPageData(page);
+            // console.log("Data=======>",dataEachPage);
+            finalResult = finalResult?.concat(dataEachPage);
+            await page.goto(`https://www.coursera.org/search?query=${category}&page=${i + 1}$&index=prod_all_launched_products_term_optimization`);
+            // await page.click("button.label-text.box.arrow[aria-label='Next Page']");
+            // await this.handleNext(page);
+          }
+
+        }
+        await this.convertToCsv(finalResult);
+        
+      })(); 
+      // console.log("All Datata====>",arr);
+      // await this.convertToCsv(arr);
+
     } catch (error) {
-      console.log('Error occured===', error);
+      console.log('Error occured===>', error);
     }
+    console.log("Final Array result ===>",arr);
   }
   async handleNext(page){
-    await page.waitForSelector(".label-text.box.arrow");
-    const next = await page.evaluate(()=>{
-      const nextPage = document.querySelectorAll(".label-text.box.arrow")[1] as HTMLElement | null;  
-      nextPage.click();
-    })
+    const select:string ="button.label-text.box.arrow[aria-label='Next Page']";
+    await page.waitForSelector(select);
+    // const next = await page.evaluate(async (select:string)=>{
+      // const nextPage = document.querySelectorAll(select)[1] as HTMLElement | null;  
+      // nextPage.click();
+      await page.click(select);
+      // return "";
+    // },select);
   }
   async convertToCsv(arr){
-    const array = [Object.keys(arr[0])].concat(arr)
-          
-            return array.map(it => {
-              return Object.values(it).toString()
-            }).join('\n');
-  }
-  async extractPageData(page){
-    const coursesLists: {
-      provider: string;
-      title: string;
-      tags: string;
-      rating: string;
-      reviews: string;
-    }[] = [];
-    const pageData = await page.evaluate(()=>{
-      async function compute() {
-        const data = document
-      .querySelectorAll(
-        '.cds-71.css-0.cds-73.cds-grid-item.cds-118.cds-126.cds-138',
-      )
-      .forEach((dta) => {
-        let temp: {
-          provider: string;
-          title: string;
-          tags: string;
-          rating: string;
-          reviews: string;
-        } = {
-          provider: '',
-          title: '',
-          tags: '',
-          rating: '',
-          reviews: '',
-        };
-        const requiredSquare = dta.querySelector(
-          '.css-1pa69gt div a div .css-ilhc4l ',
-        );
-        const temp2 = requiredSquare.querySelector('div div');
-
-        const top = temp2.querySelector(
-          '.cds-71.css-1xdhyk6.cds-73.cds-grid-item span'
-        )?.textContent || "not patched";
-        const title = temp2.querySelector('h2')?.textContent || "Not patched";
-        const tags1 = temp2.querySelector("div p")?.innerHTML || "Not patched";
-        const rating1 = requiredSquare.querySelector("div .css-pn23ng .css-zl0kzj")?.textContent || "Not patched";
-        const reviews1 = requiredSquare.querySelector("div .css-pn23ng .css-14d8ngk")?.textContent || "not patched";
-        temp.provider = top || "";
-        temp.title = title || "";
-        temp.tags = tags1.slice(tags1.indexOf("</span>")).slice(7).split(",").join("&") || "";
-        temp.rating = rating1 || "";
-        temp.reviews = reviews1 || "";
-        // console.log("Temps ==>",temp);
-        coursesLists.push(temp); 
-        // console.log(temp.tags);
-        // csvData = this.convertToCSV(coursesLists);
-      });
-    }
-    compute();
+    const keys = Object.keys(arr[0]);
+    // Create an array of strings that represents the CSV rows
+    const csvRows = arr.map(item => {
+    // Create an array of values for the current object
+      const values = keys.map(key => item[key]);
+    // Join the values with a comma
+      return values.join(",");
     });
-    return coursesLists;
+    csvRows.unshift(keys.join(","));
+    const csvString = csvRows.join("\n");
+    // Write the CSV string to a file
+    fs.writeFileSync("data.csv", csvString);
+  }
+  async extractPageData(page:any){
+    const str:string = '.cds-71.css-0.cds-73.cds-grid-item.cds-118.cds-126.cds-138';
+    await page.waitForSelector(str);
+    const pageData = await page.evaluate((str:string)=>{
+      let coursesLists: {
+        provider: string;
+        title: string;
+        tags: string;
+        rating: string;
+        reviews: string;
+      }[] = [];
+        const data = document
+                      .querySelectorAll(str)
+                      .forEach((dta) => {
+                        let temp: {
+                          provider: string;
+                          title: string;
+                          tags: string;
+                          rating: string;
+                          reviews: string;
+                        } = {
+                          provider: '',
+                          title: '',
+                          tags: '',
+                          rating: '',
+                          reviews: '',
+                        };
+                        const requiredSquare = dta.querySelector(
+                          '.css-1pa69gt div a div .css-ilhc4l'
+                        );
+                        const temp2 = requiredSquare.querySelector('div div');
+
+                        const top = temp2.querySelector(
+                          '.cds-71.css-1xdhyk6.cds-73.cds-grid-item span'
+                        )?.textContent || "";
+                        const title = temp2.querySelector('h2')?.textContent || "";
+                        const tags1 = temp2.querySelector("div p")?.innerHTML || "";
+                        const rating1 = requiredSquare.querySelector("div .css-pn23ng .css-zl0kzj")?.textContent || "";
+                        const reviews1 = requiredSquare.querySelector("div .css-pn23ng .css-14d8ngk")?.textContent || "";                                                                                                                      
+                        temp.provider = top || "";
+                        temp.title = title || "";
+                        temp.tags = tags1.slice(tags1.indexOf("</span>")).slice(7).split(",").join("&") || "";
+                        temp.rating = rating1 || "";
+                        temp.reviews = reviews1 || "";
+                        coursesLists.push(temp); 
+                      });
+      return coursesLists;
+    },str);
+    // console.log("============>Strrr",pageData);
+    return pageData;
   }
   async getPageLimit(page){
     const selector = ".pagination-controls-container > button";
-    // let totalPages = "";
     await page.waitForSelector(selector);
     const dta = await page.evaluate((selector)=>{
       const limit = document.querySelectorAll(selector);
       const req = limit[limit.length - 2];
       const totalPages = req.querySelector("span").textContent;
-      // console.log("Total page below====>",totalPages);
       return totalPages;
-      
     },selector);
     return dta;
   }
