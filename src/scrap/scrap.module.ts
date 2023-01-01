98901f7b41b97a172df87f7beb33c535da225922
Kaushal <kaushaldhakal90@gmail.com>
2023-01-01 18:23:59 +0545

    scraping coursera

--- src/scrap/scrap.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScrapController } from './scrap.controller';
-import { ScrapService } from './scrap.service';
-
-@Module({
-  controllers: [ScrapController],
-  providers: [ScrapService],
-})
-export class ScrapModule {}
